97546467fe6689b6ef9b07dcfb83028b67e1dca9
mpragnarok <mp922352612@gmail.com>
2020-07-07 07:12:32 +0800

    Add signIn and signOut methods

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -27,7 +27,6 @@ class _ChatScreenState extends State<ChatScreen> {
       final user = await _auth.currentUser();
       if (user != null) {
         loggedInUser = user;
-        print(loggedInUser.email);
       }
     } catch (e) {
       print(e);
@@ -43,7 +42,9 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                //Implement logout functionality
+                // Sign out function
+                _auth.signOut();
+                Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
